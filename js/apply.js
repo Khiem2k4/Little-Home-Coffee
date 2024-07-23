88091f1359bd76f2615aadaf702f829c2802ae0c
Khiem2k4 <manhkhiem220@gmail.com>
2024-07-23 09:07:00 +0700

    New

---
+++ js/apply.js
@@ -0,0 +1,4 @@
+document.getElementById('registrationForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Cảm ơn bạn đã đăng ký. Chúng tôi sẽ liên hệ với bạn sớm!');
+});
